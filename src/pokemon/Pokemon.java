e27df6bbc26ebd160b87eca412cb37c9e9d0f4ca
RemcoKuipers <remslaap@live.nl>
2026-05-20 09:42:31 +0200

    Superclass pokemon aangemaakt volgens de randvoorwaarden die gesteld waren (ik heb 2 extra variabelen toegevoegd)

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,4 +1,59 @@
 package pokemon;
 
-public class Pokemon {
+public abstract class Pokemon {
+    private String name;
+    private int level;
+    private int hp;
+    private int maxHp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public Pokemon(String name, int level, int hp, int maxHp) {
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+        this.maxHp = maxHp;
+
+    }
+
+    public void eat() {
+        if (this.hp < this.maxHp) {
+            this.hp += 1;
+            System.out.println("You eat + 1 HP! Current HP: " + this.hp);
+        } else {
+            System.out.println("Your HP is full (100%)");
+        }
+    }
+
+    public abstract void speaks();
 }
